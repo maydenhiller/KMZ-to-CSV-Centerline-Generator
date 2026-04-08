3a43917c654640feeea7a629072ba30aa1a6989e
Hayden Miller <222034932+maydenhiller@users.noreply.github.com>
2026-04-08 13:23:03 -0500

    Update delorme_streams.py

--- delorme_streams.py
+++ delorme_streams.py
@@ -3,7 +3,7 @@ DeLorme .dmt export helpers.
 
 A .dmt file is a proprietary Microsoft OLE “compound document” used by DeLorme / Garmin
 mapping tools. You cannot build one from lat/lon alone; this module starts from
-``template.dmt`` (a minimal blank DeLorme shell beside this file) and writes your
+a minimal blank DeLorme shell (``template.dmt`` beside this file, or embedded in this module) and writes your
 LineString geometry into it.
 
 Coordinate encoding matches GPSBabel’s DeLorme .an1 EncodeOrd / DecodeOrd.
@@ -12,14 +12,164 @@ See: https://github.com/GPSBabel/gpsbabel/blob/gpsbabel_1_7_0/an1.cc
 
 from __future__ import annotations
 
+import atexit
+import base64
 import ctypes
+import itertools
 import os
 import re
 import struct
 import tempfile
-import itertools
+import zlib
 from pathlib import Path
 from typing import Iterable, List, Optional, Sequence, Tuple
+
+# Materialized .dmt when using embedded template (Streamlit / copy-paste deploy).
+_materialized_template: Optional[Path] = None
+
+_TEMPLATE_ZLIB_B64 = (
+    'eNrt2wd0VOWi9+GdoqJYwIbdXBVBBQt2sUXsoqLYO2rUKCaaYC9g7733a++F2Luo2BV7V+y994I3'
+    '33+S4ZjDOQflXM9lfWs9j+u397x7z8ybPXtmEuLk6VFd37pw+CxvF+NYvqgq/qd18mLSDtu6lmvT'
+    'pShmyaoi/U9ra+vYzaVxZapK1WmSVLqPyVKnNHmaInVOU6ap0tRpmva7LFr5P/XbX9C0OW/TpenT'
+    'DGnG1C3NlGYuPz9KzZbLs6c50pzl7f+V9Vxp7jRP6p7mTT1SzzRfmj8tkHql3mnBtFBaOC2S+qRF'
+    '02Jp8bREWjItlZZOy6S+adm0XNtzuyhWSCum2rRS6pdWTqukVdNqafW0RlozrZX6p7XTOmndNCCt'
+    'l9ZPA9MGacO0Udo4bZI2TZulzdMWacu0Vdo6bZMGpW3Tdmn7VJd2SDumnVJ92jntkganXVNDaky7'
+    'pd1TU2pOQ9Ieac+0V9o77ZP2Tful/dMB6cA0NA1LB6WD0yHp0HRYOjwdkY5MR6Wj0zHp2HRcOj6d'
+    'kE5MJ6WT0ynp1HRaOj2dkc5MZ6Wz0znp3HReOj9dUD7/F2Z9Ubo4XZIuTZely9MV6cp0Vbo6XZOu'
+    'Tdel69MNaXhqSTemm9LN6ZZ0a7ot3Z7uSHemu9Ld6Z50b7ovjUj3pwfSg2lkeig9nB5Jj6bH0uPp'
+    'ifRkeiqNSk+nZ9Kz6bn0fHohvZheSi+nV9Kr6bX0enojvZlGp7dS6Y33nfRuei+9nz5IH6aP0sfp'
+    'k/Rp+ix9nr5IX6av0tfpm/Rt+i59n35IP6af0s/pl/RrGpN+Kz3upffZ8pt1RapMVak6TZImTZOl'
+    'TmnyNEXqnKZMU6Wp0zSpS+qapk3TpenTDGnG1C3NlGZOs6RZ02xp9jRHmrOi/PrPeq40d5ondU/z'
+    'ph6pZ5ovzZ8WSL1S77RgWigtnBZJfdKiabG0eFoiLZmWSkunZVLftGxaLi2fVkgrptq0UuqXVk6r'
+    'pFXTamn1tEZaM62V+qe10zpp3TQgrZfWTwPTBmnDtFHaOG2SNk2bpc3TFmnLtFXaOm2TBqVt03Zp'
+    '+1SXdkg7pp1Sfdo57ZIGp11TQ2pMu6XdU1NqTkPSHmnPtFfaO+2T9k37pf3TAenANDQNSwelg9Mh'
+    '6dB0WDo8HZGOTEelo9Mx6dh0XDo+nZBOTCelk9Mp6dR0Wjo9nZHOTGels9M56dx0Xjo/XVA+/xdm'
+    'fVG6OF2SLk2XpcvTFenKdFW6Ol2Trk3XpevTDWl4akk3ppvSzemWdGu6Ld2e7kh3prvS3emedG+6'
+    'L41I96cH0oNpZHooPZweSY+mx9Lj6Yn0ZHoqjUpPp2fSs+m59Hx6Ib2YXkovp1fSq+m19Hp6I72Z'
+    'Rqe30tvpnfRuei+9nz5IH6aP0sfpk/Rp+ix9nr5IX6av0tfpm/Rt+i59n35IP6af0s/pl/RrGpN+'
+    'Kz3uqbX8g1pFqkxVqTpNkiZNk6VOafI0ReqcpkxTpanTNKlL6pqmTdOl6dMMacbULc2UZk6zpFnT'
+    'bGn2NEeas7L8+s96rjR3mid1T/OmHqlnmi/NnxZIvVLvtGBaKC2cFkl90qJpsbR4WiItmZZKS6dl'
+    'Ut+0bFouLZ9WSCum2rRS6pdWTqukVdNqafW0RlozrZX6p7XTOmndNCCtl9ZPA9MGacO0Udo4bZI2'
+    'TZulzdMWacu0Vdo6bZMGpW3Tdmn7VJd2SDumnVJ92jntkganXVNDaky7pd1TU2pOQ9Ieac+0V9o7'
+    '7ZP2Tful/dMB6cA0NA1LB6WD0yHp0HRYOjwdkY5MR6Wj0zHp2HRcOj6dkE5MJ6WT0ynp1HRaOj2d'
+    'kc5MZ6Wz0znp3HReOj9dUD7/F2Z9Ubo4XZIuTZely9MV6cp0Vbo6XZOuTdel69MNaXhqSTemm9LN'
+    '6ZZ0a7ot3Z7uSHemu9Ld6Z50b7ovjUj3pwfSg2lkeig9nB5Jj6bH0uPpifRkeiqNSk+nZ9Kz6bn0'
+    'fHohvZheSi+nV9Kr6bX0enojvZlGp7fS2+md9G56L72fPkgfpo/Sx+mT9Gn6LH2evkhfpq/S1+mb'
+    '9G36Ln2ffkg/pp/Sz+mX9Gsak34rPe6ptfyPtIpUmapSdZokTZomS53S5GmK1DlNmaZKU6dpUpfU'
+    'NU2bpkvTpxnSjKlbminNnGZJs6bZ0uxpjjRnVfn1n/Vcae40T+qe5k09Us80X5o/LZB6pd5pwbRQ'
+    'WjgtkvqkRdNiafG0RFoyLZWWTsukvmnZtFxaPq2QVky1aaXUL62cVkmrptXS6mmNtGZaK/VPa6d1'
+    '0rppQFovrZ8Gpg3ShmmjtHHaJG2aNkubpy3SlmmrtHXaJg1K26bt0vapLu2Qdkw7pfq0c9olDU67'
+    'pobUmHZLu6em1JyGpD3SnmmvtHfaJ+2b9kv7pwPSgWloGpYOSgenQ9Kh6bB0eDoiHZmOSkenY9Kx'
+    '6bh0fDohnZhOSienU9Kp6bR0ejojnZnOSmenc9K56bx0frqgfP4vzPqidHG6JF2aLkuXpyvSlemq'
+    'dHW6Jl2brkvXpxvS8NSSbkw3pZvTLenWdFu6Pd2R7kx3pbvTPenedF8ake5PD6QH08j0UHo4PZIe'
+    'TY+lx9MT6cn0VBqVnk7PpGfTc+n59EJ6Mb2UXk6vpFfTa+n19EZ6M41Ob6W30zvp3fReej99kD5M'
+    'H6WP0yfp0/RZ+jx9kb5MX6Wv0zfp2/Rd+j79kH5MP6Wf0y/p1zQm/VZ63FNr+Rc0FakyVaXqNEma'
+    'NE2WOqXJ0xSpc5oyTZWmTtOkLqlrmjZNl6ZPM6QZU7c0U5o5zZJmTbOl2dMcac7q8us/67nS3Gme'
+    '1D3Nm3qknmm+NH9aIPVKvdOCaaG0cFok9UmLpsXS4mmJtGRaKi2dlkl907JpubR8WiGtmGrTSqlf'
+    'WjmtklZNq6XV0xppzbRW6p/WTuukddOAtF5aPw1MG6QN00Zp47RJ2jRtljZPW6Qt01Zp67RNGpS2'
+    'Tdul7VNd2iHtmHZK9WnntEsanHZNDakx7ZZ2T02pOQ1Je6Q9015p77RP2jftl/ZPB6QD09A0LB2U'
+    'Dk6HpEPTYenwdEQ6Mh2Vjk7HpGPTcen4dEI6MZ2UTk6npFPTaen0dEY6M52Vzk7npHPTeen8dEH5'
+    '/F+Y9UXp4nRJujRdli5PV6Qr01Xp6nRNujZdl65PN6ThqSXdmG5KN6db0q3ptnR7uiPdme5Kd6d7'
+    '0r3pvjQi3Z8eSA+mkemh9HB6JD2aHkuPpyfSk+mpNCo9nZ5Jz6bn0vPphfRieim9nF5Jr6bX0uvp'
+    'jfRmGp3eSm+nd9K76b30fvogfZg+Sh+nT9Kn6bP0efoifZm+Sl+nb9K36bv0ffoh/Zh+Sj+nX9Kv'
+    'aUz6rfS4p9byL2crUmWqStVpkjRpmix1SpOnKVLnNGWaKk2dpkldUtc0bZouTZ9mSDOmbmmmNHOa'
+    'Jc2aZkuzpznSnJOUX/9Zz5XmTvOk7mne1CP1TPOl+dMCqVfqnRZMC6WF0yKpT1o0LZYWT0ukJdNS'
+    'aem0TOqblk3LpeXTCmnFVJtWSv3SymmVtGpaLa2e1khrprVS/7R2Wietmwak9dL6aWDaIG2YNkob'
+    'p03SpmmztHnaIm2Ztkpbp23SoLRt2i5tn+rSDmnHtFOqTzunXdLgtGtqSI1pt7R7akrNaUjaI+2Z'
+    '9kp7p33Svmm/tH86IB2YhqZh6aB0cDokHZoOS4enI9KR6ah0dDomHZuOS8enE9KJ6aR0cjolnZpO'
+    'S6enM9KZ6ax0djonnZvOS+enC8rn/8KsL0oXp0vSpemydHm6Il2ZrkpXp2vStem6dH26IQ1PLenG'
+    'dFO6Od2Sbk23pdvTHenOdFe6O92T7k33pRHp/vRAejCNTA+lh9Mj6dH0WHo8PZGeTE+lUenp9Ex6'
+    'Nj2Xnk8vpBfTS+nl9Ep6Nb2WXk9vpDfT6PRWeju9k95N76X30wfpw/RR+jh9kj5Nn6XP0xfpy/RV'
+    '+jp9k75N36Xv0w/px/RT+jn9kn5NY9Jvpcc9tZb/x0xFqkxVqTpNkiZNk6VOafI0ReqcpkxTpanT'
+    'NKlL6pqmTdOl6dMMacbULc2UZk6zpFnTbGn2NEeac9Ly6z/rudLcaZ7UPc2beqSeab40f1og9Uq9'
+    '04JpobRwWiT1SYumxdLiaYm0ZFoqLZ2WSX3Tsmm5tHxaIa2YatNKqV9aOa2SVk2rpdXTGmnNtFbq'
+    'n9ZO66R104C0Xlo/DUwbpA3TRmnjtEnaNG2WNk9bpC3TVmnrtE0alLZN26XtU13aIe2Ydkr1aee0'
+    'Sxqcdk0NqTHtlnZPTak5DUl7pD3TXmnvtE/aN+2X9k8HpAPT0DQsHZQOToekQ9Nh6fB0RDoyHZWO'
+    'TsekY9Nx6fh0QjoxnZROTqekU9Np6fR0RjoznZXOTuekc9N56fx0Qfn8X5j1RenidEm6NF2WLk9X'
+    'pCvTVenqdE26Nl2Xrk83pOGpJd2Ybko3p1vSrem2dHu6I92Z7kp3p3vSvem+NCLdnx5ID6aR6aH0'
+    'cHokPZoeS4+nJ9KT6ak0Kj2dnknPpufS8+mF9GJ6Kb2cXkmvptfS6+mN9GYand5Kb6d30rvpvfR+'
+    '+iB9mD5KH6dP0qfps/R5+iJ9mb5KX6dv0rfpu/R9+iH9mH5KP6df0q9pTPqt9Lin1vL/lK1Ilakq'
+    'VadJ0qRpstQpTZ6mSJ3TlGmqNHWaJnVJXdO0abo0fZohzZi6pZnSzGmWNGuaLc2e5khzTlZ+/Wc9'
+    'V5o7zZO6p3lTj9QzzZfmTwukXql3WjAtlBZOi6Q+adG0WFo8LZGWTEulpdMyqW9aNi2Xlk8rpBVT'
+    'bVop9Usrp1XSqmm1tHpaI62Z1kr909ppnbRuGpDWS+ungWmDtGHaKG2cNkmbps3S5mmLtGXaKm2d'
+    'tkmD0rZpu7R9qks7pB3TTqk+7Zx2SYPTrqkhNabd0u6pKTWnIWmPtGfaK+2d9kn7pv3S/umAdGAa'
+    'moalg9LB6ZB0aDosHZ6OSEemo9LR6Zh0bDouHZ9OSCemk9LJ6ZR0ajotnZ7OSGems9LZ6Zx0bjov'
+    'nZ8uKJ//C7O+KF2cLkmXpsvS5emKdGW6Kl2drknXpuvS9emGNDy1pBvTTenmdEu6Nd2Wbk93pDvT'
+    'XenudE+6N92XRqT70wPpwTQyPZQeTo+kR9Nj6fH0RHoyPZVGpafTM+nZ9Fx6Pr2QXkwvpZfTK+nV'
+    '9Fp6Pb2R3kyj01vp7fROeje9l95PH6QP00fp4/RJ+jR9lj5PX6Qv01fp6/RN+jZ9l75PP6Qf00/p'
+    '5/RL+jWNSb+VHvfUWv5ARkWqTFWpOk2SJk2TpU5p8jRF6pymTFOlqdM0qUvqmqZN06Xp0wxpxtQt'
+    'zZRmTrOkWdNsafY0R5qzU/v5Z+Jav2jMf0OKmmKVoiHrprZPDPx5MxaTVIy9r4o/eZtZS5+96NR+'
+    'eeWiruifr6Cp2DWX+uXSrsVuWTZkVPp6mv/gvuYpKv42f9W/uM5Xs3702EEPv17x349+3Lbu+Hml'
+    'v59/waI2sza0PSKDUmnLxm17d8lXslu2bdf2CY3fLdFh/m5/MP/Y9fjn3yPzNhbrZK49/3D20mde'
+    'Kko/wrd95qrrXzD/2pllt6JPti/Y9tmaP1LT4fin+QvmH5BLdcUOqantGVA64ubxfC29ioq/fWZs'
+    '6r9o/vq2Z16fP/UIzJX5q8ufb5vqL5h/gzz+TcWOuTwkl0uvzcF/dPylX+G0fbZuir9g/k2yZVDb'
+    's/+Pn3tF22ewfn/8J/+D+U+86B9ffx3nWzWP/OC2sz6o7atp/lPPv8qKju9nf/Z9q/z287fHuF8a'
+    'lBmbi4Ftr7+mjHb8p0f8D+f/D+cf3+O/brFtsXPm2S6zlt799mh77v15M+WVP6HHX9Hh+DvO31xM'
+    'uC4dzv+/c/wDyo/zGjnuHdo+1TZhps35n7L8edI/e/yV5c8Wts/f/n7TL498U/k9Z0ieA6XX35C2'
+    'd4Idx/u49Pw3nn+l71NXl79Z/f5+O/Bv33Mm7Ptv+/xdJuD4py1/RvJfzd/nP/z8m67D/H///vP3'
+    '3/3Gvh/81Y//9OXPphZtn/8c9/t9bdtroT7z1/2Jd6Q+/8bxzzD2RfhP55+wd8F5M/9s5c/U/tn5'
+    'Zx77Imh7/Zee9zV5/vf/27Jnjmq+P3n+Z/s3Hv+a4vcHYN0c9U5t3+3Hzr7IBH0d82T+6cufOf6z'
+    '8//3eOfvM8HzT/DxV4xv/kUneP4Zyj9T/+njH+/8i/3nj79yfPMvPsHzl17Ls0zI8Y93/iX+88df'
+    'Nb75l5zQ+dtez3NOyPGPd/6l/q3jn30C5q+pHt/8S0/w8U/o4//f1eN//1l4Ar6Cef+d8z/J+Odf'
+    'ZALnL30//a8JOf9/MH+f//TxTzr++RedsPnbvp/MPSHH/wfzLzaB80/w8U82/vkXn8DHf67yz3R/'
+    '+vg7zD/2b3NK/Tt/r9V1nPso1fHvf0rjWcrfn8b9+5+awu8AJ7Zdiv+div+D24/vOtXl5+uQHuWf'
+    '7Xu0X3+68nra8rpreT1P1t3L1y39rdBKg5rratYetFuvlfZorm+oa26uGdBY3zCkuaZxh5o1GobU'
+    'NdU1D6npufagnRub5uvVr3GPhiH71KzU2LR9XVNzr1X2rh/S3KvtWg2DhtQ3NgwaXNN/0LZ1g5t7'
+    'DRg8aLu65tywvqF0w/Hc5/qNg7b//YobNm07qKGmtqluUE2/xsGNTb16r7x2/637bThwg3XX3nrV'
+    'NfpvsMr6W6+x8tiDHzX0sG26HvZB6e/YinW2Xb629Mdt0y17UKfp04ypW/qvNFeaJ3VPPdLCacU0'
+    'IG2TTkzfpG/Tj+mnNCb9llpTxXIHdapONWlA2j01peY0JO2R9kx7pbdS5fK57sqZJw1I26Ri9dQ/'
+    'zVTZtZhykq5dukyS946puxadpunaZfpDuhYDDunapfqwrjVp4bRiGpB2S8NSURzddb1Ox3TdJnWt'
+    'PrbrIqkoTkiHzlT6g72BjTsM2WtQU90WK9f1b2zatW6LgbV9Fl5k8bHDfo277tbYUJezMHbLgu1X'
+    'WHBAU90OOSUNOV8LLlL6NlLeXzNwSFNd3ZCa2iGDBzXXbDiw9rqa0vVrBgzeo7n0V0qrNdVv39yr'
+    '9Nyp6VdXOqs1/Zoam5t3GlTf1Kum/alRs25DXc3Gg/bJpXGeEDV/f9rH96T75/vKt/szz9o/uFL5'
+    'njZo3Kvhb8/smnGe6TX/9Ele8w/P1prN1hjQe0Bj85BcqV/j9nXNnTq8xP7l72KuHjXV6rd9fM4u'
+    'v//+9KsV/n6d127XDlf8C28/9v8Z9O4z5f/6vQzg/yelv2Ps3vv399LSsqL95/3anj3Gbq9s/4m+'
+    '/Q1yzPAR9R3Xbf+abvunR9U/vLlXdvjxraUohjU0lscja4qWvqOHlsbV47wVAwAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMBGMqiiK'
+    '7r3bL1eWl9lUdC6K2p49xm4v7elWtO0oijHDR9R3XFeXFq2trUVRNc6dt9+wojxqKYphDY3l8cia'
+    'oqXv6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3bL1eWl9lUdC6K2p49xm4v7elWtO0oijHDR9R3XFeX'
+    'Fq2trUVRNc6dt9+wojxqKYphDY3l8ciaoqXv6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3bL1eWl9lU'
+    'dC6K2p49xm4v7elWtO0oijHDR9R3XFeXFq2trUVRNc6dt9+wojxqKYphDY3l8ciaoqXv6KGlcXV5'
+    'f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAMPGMqiiK7r3bL1eWl9lUdC6K2p49xm4v7elWtO0oijHDR9R3XFeXFq2trUVRNc6d'
+    't9+wojxqKYphDY3l8ciaoqXv6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3bL1eWl9lUdC6K2p49xm4v'
+    '7elWtO0oijHDR9R3XFeXFq2trUVRNc6dt9+wojxqKYphDY3l8ciaoqXv6KGlcXV5f5WzAQAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'MPGMqiiK7r3bL1eWl9lUdC6K2p49xm4v7elWtO0oijHDR9R3XFeXFq2trUVRNc6dt9+wojxqKYph'
+    'DY3l8ciaoqXv6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3bL1eWl9lUdC6K2p49xm4v7elWtO0oijHD'
+    'R9R3XFeXFq2trUVRNc6dt9+wojxqKYphDY3l8ciaoqXv6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3b'
+    'L1eWl9lUdC6K2p49xm4v7elWtO0oijHDR9R3XFeXFq2trUVRNc6dt9+wojxqKYphDY3l8ciaoqXv'
+    '6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3bL1eWl9lUdC6K2p49xm4v7elWtO0oijHDR9R3XFeXFq2t'
+    'rUVRNc6dt9+wojxqKYphDY3l8ciaoqXv6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3bL1eWl9lUdC6K'
+    '2p49xm4v7elWtO0oijHDR9R3XFeXFq2trUVRNc6dt9+wojxqKYphDY3l8ciaoqXv6KGlcXV5f5Wz'
+    'AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAMPGMqiiK7r3bL1eWl9lUdC6K2p49xm4v7elWtO0oijHDR9R3XFeXFq2trUVRNc6dt9+w'
+    'ojxqKYphDY3l8ciaoqXv6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3bL1eWl9lUdC6K2p49xm4v7elW'
+    'tO0oijHDR9R3XFeXFq2trUVRNc6dt9+wojxqKYphDY3l8ciaoqXv6KGlcXV5f5WzAQAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMPGM'
+    'qiiK7r3bL1eWl9lUdC6K2p49xm4v7elWtO0oijHDR9R3XFeXFq2trUVRNc6dt9+wojxqKYphDY3l'
+    '8ciaoqXv6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3bL1eWl9lUdC6K2p49xm4v7elWtO0oijHDR9R3'
+    'XFeXFq2trUVRNc6dt9+wojxqKYphDY3l8ciaoqXv6KGlcXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMPGMqiiK7r3bL1eW'
+    'l9lUdC6K2p49xm4v7elWtO0oijHDR9R3XFeXFq2trUVRNc6dt9+wojxqKYphDY3l8ciaoqXv6KGl'
+    'cXV5f5WzAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAAAAAAAAAAAAAAMPH8P0Bu75w='
+)
+
+
+def _embedded_template_dmt_bytes() -> bytes:
+    """Decompress the blank-based shell shipped inside this module."""
+    return zlib.decompress(base64.b64decode("".join(_TEMPLATE_ZLIB_B64)))
+
 
 def kml_abgr_to_colorref(kml_color: Optional[str]) -> int:
     """
@@ -230,16 +380,37 @@ def resolve_template_dmt_path() -> Path:
     """
     Path to the DeLorme OLE shell used for .dmt export.
 
-    Expects ``template.dmt`` next to this module (see repo). Regenerate from ``blank.dmt``
-    using ``scripts/build_template_from_blank.py`` if you replace the blank shell.
+    **Order:** (1) ``template.dmt`` beside this file if present (local override);
+    (2) otherwise the shell embedded in this module (zlib+base64), written once to a
+    temp file. No separate Git/binary step is required for Streamlit if you only ship
+    ``delorme_streams.py``.
     """
+    global _materialized_template
+
     p = template_dmt_path()
     if p.is_file():
         return p
-    raise FileNotFoundError(
-        f"Missing {p.name} next to delorme_streams.py. "
-        "Copy the repository template or run scripts/build_template_from_blank.py."
-    )
+
+    if _materialized_template is not None and _materialized_template.is_file():
+        return _materialized_template
+
+    raw = _embedded_template_dmt_bytes()
+    fd, name = tempfile.mkstemp(suffix=".dmt", prefix="kmz_cl_template_")
+    try:
+        os.write(fd, raw)
+    finally:
+        os.close(fd)
+    tmp_path = Path(name)
+    _materialized_template = tmp_path
+
+    def _cleanup(path: Path = tmp_path) -> None:
+        try:
+            path.unlink(missing_ok=True)
+        except OSError:
+            pass
+
+    atexit.register(_cleanup)
+    return tmp_path
 
 
 _ANNOTATE_WORKSPACE = "DeLormeComponents/DeLorme.Annotate.Workspace"
